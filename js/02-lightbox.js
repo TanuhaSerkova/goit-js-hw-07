a4bb07a49132e2ef42dc12cd85501bfce48d48de
Tatiana Sierkova <tanuha_86@ukr.net>
2022-02-05 18:45:20 +0200

    01

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryBox = galleryItems.map(({ preview, original, description }) => { 
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}",
+        data-source="${original}",
+        alt="${description}",
+        />
+    </a>
+</div>`;
+}).join("");
+
+const galleryHandler = (event) => { 
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+    const originalUrl = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+        <img src="${originalUrl}" >
+    `)
+    instance.show();
+};
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryBox);
+galleryEl.addEventListener("click", galleryHandler);
+
+
