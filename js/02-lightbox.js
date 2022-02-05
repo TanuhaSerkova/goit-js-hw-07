9055ed94260a79f3060171c30815e0375f8dfa15
Tatiana Sierkova <tanuha_86@ukr.net>
2022-02-06 00:15:07 +0200

    task02-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,34 +1,39 @@
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
+// Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt.
+// Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-const galleryEl = document.querySelector('.gallery');
 
-const galleryBox = galleryItems.map(({ preview, original, description }) => { 
-    return `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
+const galleryEl = document.querySelector('.gallery'); 
+
+// 1. Создание и рендер разметки по массиву данных galleryItems
+
+const galleryMarkup = galleryItems.map(({ original, preview, description }) => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
         <img
-        class="gallery__image"
-        src="${preview}",
-        data-source="${original}",
-        alt="${description}",
-        />
-    </a>
-</div>`;
-}).join("");
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`)
+    .join("");
 
-const galleryHandler = (event) => { 
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") return;
-    const originalUrl = event.target.dataset.source;
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
-    const instance = basicLightbox.create(`
-        <img src="${originalUrl}" >
-    `)
-    instance.show();
+console.log(galleryEl);
+
+//  3. Инициализация библиотеки после того, как элементы галереи созданы и добавлены в `div.gallery`.
+
+const lightboxOptions = {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
 };
 
-galleryEl.insertAdjacentHTML("afterbegin", galleryBox);
-galleryEl.addEventListener("click", galleryHandler);
-
-
+const lightboxGallery = new SimpleLightbox(".gallery a", lightboxOptions);
